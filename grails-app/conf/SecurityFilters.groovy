c42b50cee6da2466a499691b6c2a27ec4670f778
ramirin90 <ramiroherreraruiz@gmail.com>
2016-04-25 21:48:30 -0500

    little change int eh filters

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -4,8 +4,8 @@ class SecurityFilters {
 			before = {
 				if (!controllerName)
 					return true
-				def allowedActions = ['show', 'index', 'login',
-									  'validate', 'search']
+				def allowedActions = ['show', 'index', 'login','validate', 'search']
+//				def allowedActions = ['show','login','validate', 'search']
 				if (!session.user && !allowedActions.contains(actionName)){
 					redirect(controller:'recruiter', action:'login',
 							 params:['cName': controllerName,
